fb6ea01213579fcfe3d134b4ff386d98391f1ca2
rishavs <rishav.sharan@live.com>
2017-03-07 19:33:37 +0530

    chose orm = gorm. basic conn done

--- src/main.go
+++ src/main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
+	"log"
+	// "net/http"
 	// "strconv"
+	// "database/sql"
+	// _ "github.com/lib/pq"
 
+	"github.com/spf13/viper"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+    "github.com/jinzhu/gorm"
+    _ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 //------------------------------------
 // Model
 //------------------------------------
+
+
 
 //------------------------------------
 // Main
@@ -18,16 +27,74 @@ import (
 
 func main() {
 
-	data, err := ioutil.ReadFile("config.yml")
-	checkErr(err)
+	//------------------------------------
+	// Read Config
+	//------------------------------------
+
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil { 
+	    panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+
+	db_uri, ok := viper.Get("PG_URL").(string)
+	if ok != true {log.Fatal(err)}
+
+	fmt.Println(db_uri)
+
+	//------------------------------------
+	// Connect to Db
+	//------------------------------------
+
+	db, err := gorm.Open("postgres", db_uri)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("Connected successfully to DB")
+	}
+
+	defer db.Close()
+
+	type Post struct {
+		gorm.Model
+
+		Title string
+		Content string
+		User User
+		UserId uint
+	}
+
+	type User struct {
+	    gorm.Model
+
+	    Name string
+	}
+
+	db.Model(&post).Related(&user)
+
+	//------------------------------------
+	// Create Server
+	//------------------------------------
 
 	e := echo.New()
 
-	// Middleware
+	//------------------------------------
+	// Register Middlewares
+	//------------------------------------
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
+	//------------------------------------
 	// Routes
+	//------------------------------------
 
-	e.Logger.Fatal(e.Start(":3000"))
+
+
+
+	//------------------------------------
+	// Start Server
+	//------------------------------------
+	// e.Logger.Fatal(e.Start(":3000"))
 }
+
